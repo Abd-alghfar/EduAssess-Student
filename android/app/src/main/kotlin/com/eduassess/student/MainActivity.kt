033ac117bf9a8de046aa5178550186cb176391d5
3bdalghfar <a.getany1@gmail.com>
2026-03-10 00:46:59 +0200

    feat: add screenshot prevention (FLAG_SECURE) for Android

--- android/app/src/main/kotlin/com/eduassess/student/MainActivity.kt
+++ android/app/src/main/kotlin/com/eduassess/student/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.eduassess.student
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.view.WindowManager
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
